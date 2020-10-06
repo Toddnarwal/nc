e5895917d9ffe4d627d21912e6f44203e5fac2d9
Toddnarwal <todd.thomas@comcast.net>
2020-10-06 08:13:45 -0700

    Update portainer-setup.sh

--- install_scripts/portainer/portainer-setup.sh
+++ install_scripts/portainer/portainer-setup.sh
@@ -2,8 +2,8 @@
 
 echo ''
 echo 'STEP 0'
-sudo docker volume create portainer_data
+docker volume create portainer_data
 
 echo ''
 echo 'STEP 1'
-sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer-ce --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+docker run -d -p 8000:8000 -p 9000:9000 --name=portainer-ce --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
