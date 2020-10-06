44a722941a8e7830828a4a3805cd6daa7779a536
Toddnarwal <todd.thomas@comcast.net>
2020-10-06 08:13:09 -0700

    Update portainer-remove.sh

--- install_scripts/portainer/portainer-remove.sh
+++ install_scripts/portainer/portainer-remove.sh
@@ -2,12 +2,12 @@
 
 echo ''
 echo 'STEP 0'
-sudo docker stop portainer-ce
+docker stop portainer-ce
 echo ''
 echo 'STEP 1'
-sudo docker rm portainer-ce
+docker rm portainer-ce
 echo ''
 echo 'STEP 2'
-sudo docker volume prune -f
+docker volume prune -f
 
 
