8abf72dbc0113b1ccbab844e0c66bcd02ec9f86c
Toddnarwal <todd.thomas@comcast.net>
2020-10-06 08:42:26 -0700

    Update docker-remove.sh

--- install_scripts/docker/docker-remove.sh
+++ install_scripts/docker/docker-remove.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-echo ''
-echo 'STEP 0'
-sudo rm /usr/local/bin/docker-compose
-echo ''
-echo 'STEP 1'
-sudo apt-get remove -y docker docker-engine docker.io containerd runc
-echo ''
-echo 'STEP 2'
-sudo rm -rf /var/lib/docker/
 
+sudo rm /usr/local/bin/docker-compose
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
+sudo rm -rf /var/lib/docker/
