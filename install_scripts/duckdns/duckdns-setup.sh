25872d12babc09ca9ba2d41468f888bf871ad7eb
Toddnarwal <todd.thomas@comcast.net>
2020-10-06 07:57:45 -0700

    Update duckdns-setup.sh

--- install_scripts/duckdns/duckdns-setup.sh
+++ install_scripts/duckdns/duckdns-setup.sh
@@ -2,11 +2,11 @@
 
 echo ''
 echo 'STEP 0'
-sudo docker volume create duckdns-tdouble_config
+docker volume create duckdns-tdouble_config
 
 echo ''
 echo 'STEP 1'
-sudo docker create \
+docker create \
   --name=duckdns-tdouble \
   --env-file="../env" \
   -v duckdns-tdouble_config:/config  \
