88f394e54629b890cee563ce3c4497dfbb5c1129
Toddnarwal <todd.thomas@comcast.net>
2020-10-14 10:51:45 -0700

    Update portainer-setup.sh

--- install_scripts/portainer/portainer-setup.sh
+++ install_scripts/portainer/portainer-setup.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
-# Create SSL certs for HTTPS
+#
+# Create SSL certs for HTTPS access to Portainer
+#
 mkdir local-certs && cd local-certs
 openssl genrsa -out portainer.key 2048
 openssl ecparam -genkey -name secp384r1 -out portainer.key
 openssl req -new -x509 -sha256 -key portainer.key -out portainer.crt -days 3650
+
+#
+# Create SSL certs to access to remote Docker port
+#
+openssl genrsa -out docker-key.pem 4096
+openssl req -subj '/CN=client' -new -key docker-key.pem -out client.csr
+echo extendedKeyUsage = clientAuth > extfile-client.cnf
+
+
+# ca.pem and ca-key.pem are generated after running docker-setup.sh
+# Copy ca.pem and ca-key.pem from remote docker server
+openssl x509 -req -days 365 -sha256 -in client.csr -CA ca.pem -CAkey ca-key.pem \
+  -CAcreateserial -out docker-cert.pem -extfile extfile-client.cnf
+# docker-cert.pem and docker-key.pem are used to configure remote docker access from Portainer GUI
+
+#
+# Clean up
+#
+rm extfile-client.cnf client.csr
 
 # Installing Portainer
 docker volume create portainer_data
