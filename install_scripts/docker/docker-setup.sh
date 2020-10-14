1ec80ba2202d8cb4fc0a4a3ad078adad96a8c05d
Toddnarwal <todd.thomas@comcast.net>
2020-10-14 09:29:37 -0700

    Update docker-setup.sh

--- install_scripts/docker/docker-setup.sh
+++ install_scripts/docker/docker-setup.sh
@@ -2,10 +2,10 @@
 
 #
 # Create certs to secure docker port for remote management
-#
+# 
 
 export HOST=<your-domain-name>
-mkdir ~/.certs
+mkdir -p ~/.certs
 
 openssl genrsa -aes256 -out ~/.certs/ca-key.pem 4096
 # enter a pass phrase to protect the ca-key
@@ -16,13 +16,20 @@ openssl req -new -x509 -days 365 -key ~/.certs/ca-key.pem -sha256 \
 
 openssl genrsa -out ~/.certs/server-key.pem 4096
 openssl req -subj "/CN=$HOST" -sha256 -new -key ~/.certs/server-key.pem \
- -out ~/.certs/server.csr
-echo subjectAltName = DNS:$HOST,IP:127.0.0.1 >> ~/.certs/extfile.cnf
-echo extendedKeyUsage = serverAuth >> ~/.certs/extfile.cnf
-openssl x509 -req -days 365 -sha256 -in ~/.certs/server.csr -CA ~/.certs/ca.pem \
+ -out server.csr
+echo subjectAltName = DNS:$HOST,IP:127.0.0.1 >> extfile.cnf
+echo extendedKeyUsage = serverAuth >> extfile.cnf
+openssl x509 -req -days 365 -sha256 -in server.csr -CA ~/.certs/ca.pem \
  -CAkey ~/.certs/ca-key.pem -CAcreateserial -out ~/.certs/server-cert.pem \
- -extfile ~/.certs/extfile.cnf
+ -extfile extfile.cnf
 # enter a pass phrase to create server-cert.pem
+
+#
+# Clean up
+#
+chmod -v 0400  ~/.certs/ca-key.pem  ~/.certs/server-key.pem
+chmod -v 0444  ~/.certs/ca.pem  ~/.certs/server-cert.pem
+rm extfile.cnf server.csr ~/.certs/ca.srl
 
 #
 # Override docker startup options
@@ -36,6 +43,7 @@ echo ExecStart= >> startup_options.conf
 echo ExecStart=/usr/bin/dockerd --tlsverify --tlscacert=$HOME/.certs/ca.pem \
  --tlscert=$HOME/.certs/server-cert.pem --tlskey=$HOME/.certs/server-key.pem \
   -H fd:// -H tcp://0.0.0.0:2376 >> startup_options.conf
+cp startup_options.conf /etc/systemd/system/docker.service.d/startup_options.conf
 
 #
 # Install docker
