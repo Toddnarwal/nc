495cccad3ae48dfe3f099cf08cfcb3fe82077df8
Toddnarwal <todd.thomas@comcast.net>
2020-10-14 09:35:27 -0700

    Update docker-remove.sh

--- install_scripts/docker/docker-remove.sh
+++ install_scripts/docker/docker-remove.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo systemctl stop docker.service
+sudo rm /etc/systemd/system/docker.service.d/startup_options.conf
 sudo rm /usr/local/bin/docker-compose
 sudo apt-get remove -y docker docker-engine docker.io containerd runc
 sudo rm -rf /var/lib/docker/
+sudo rm -rf ~/.certs
