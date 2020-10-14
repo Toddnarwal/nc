47714226dbc0554673890f1ba1a67f121435b57e
Toddnarwal <todd.thomas@comcast.net>
2020-10-14 14:15:26 -0700

    Update restrict_onlyoffice.sh

--- install_scripts/onlyoffice/restrict_onlyoffice.sh
+++ install_scripts/onlyoffice/restrict_onlyoffice.sh
@@ -4,6 +4,6 @@
 # Restrict onlyoffice to nextcloud instance
 #
 
-docker exec -it onlyoffice sed -i 's/"rules": \[{"address": "\*", "allowed": true}\],/"rules": \[{"address": "<nextcloud.server.com>", "allowed": true}\],/g' /etc/onlyoffice/documentserver/default.json
+docker exec -it onlyoffice sed -i 's/"rules": \[{"address": "\*"/"rules": \[{"address": "<nextcloud.server.com>"/g' /etc/onlyoffice/documentserver/default.json
 
 supervisorctl restart all
