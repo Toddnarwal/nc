df2baedd07394f2c1bca1a28ff862e5efab60ece
Toddnarwal <todd.thomas@comcast.net>
2020-10-14 20:11:19 -0700

    Update restrict_onlyoffice.sh

--- install_scripts/onlyoffice/restrict_onlyoffice.sh
+++ install_scripts/onlyoffice/restrict_onlyoffice.sh
@@ -4,6 +4,13 @@
 # Restrict onlyoffice to nextcloud instance
 #
 
-docker exec -it onlyoffice sed -i 's/"rules": \[{"address": "\*"/"rules": \[{"address": "<nextcloud.server.com>"/g' /etc/onlyoffice/documentserver/default.json
+docker exec -it onlyoffice sed -i 's/"string": "Secret"/"string": "Secret"/g' /etc/onlyoffice/documentserver/default.json
+docker exec -it onlyoffice sed -i 's/"browser": false/"browser": true/g' /etc/onlyoffice/documentserver/default.json
+docker exec -it onlyoffice sed -i 's/"inbox": false/"inbox": true/g' /etc/onlyoffice/documentserver/default.json
+docker exec -it onlyoffice sed -i 's/"outbox": false/"outbox": true/g' /etc/onlyoffice/documentserver/default.json
+docker exec -it onlyoffice sed -i 's/false/true/g' /etc/onlyoffice/documentserver/local.json
+
+# Choose Secret key to be used in Nextcloud GUI for Onlyoffice configuration
+docker exec -it onlyoffice sed -i 's/"secret"/"supersecretpassword"/g' /etc/onlyoffice/documentserver/local.json
 
 docker exec -it onlyoffice  supervisorctl restart all
