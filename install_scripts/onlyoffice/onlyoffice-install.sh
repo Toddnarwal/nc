451d028217243178d13de93157cd4db6a02641f4
Toddnarwal <todd.thomas@comcast.net>
2020-10-14 17:52:34 -0700

    Update onlyoffice-install.sh

--- install_scripts/onlyoffice/onlyoffice-install.sh
+++ install_scripts/onlyoffice/onlyoffice-install.sh
@@ -16,4 +16,4 @@ sudo docker run -i -t -d --restart=always --name onlyoffice -p 8080:80 \
   -v onlyoffice_rabbitmq:/var/lib/rabbitmq \
   -v onlyoffice_redis:/var/lib/redis \
   -v onlyoffice_log:/var/log/onlyoffice \
-  onlyoffice/documentserver
+  onlyoffice/documentserver:5.4.2.46
