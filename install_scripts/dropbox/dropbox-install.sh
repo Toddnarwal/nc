83a5c3a8bf51459c98e2e9edc3311e67ae7f2e8e
Toddnarwal <todd.thomas@comcast.net>
2020-10-17 12:32:58 -0700

    Update dropbox-install.sh

--- install_scripts/dropbox/dropbox-install.sh
+++ install_scripts/dropbox/dropbox-install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Use a local volume to share directory with Nextcloud
+docker volume create dropbox_data
+
 docker run --detach --name=dropbox --restart=always \
   -e DROPBOX_UID=$(id -u) -e DROPBOX_GID=$(id -g) \
   -v "$HOME:/opt/dropbox/.dropbox" \
-  -v "$HOME:/opt/dropbox/Dropbox" \
+  -v "dropbox_data:/opt/dropbox/Dropbox" \
   otherguy/dropbox:latest
   
 # Check logs for dropbox url to allow access:  docker logs --follow dropbox
