d9140576e6ac8863e16706b97ebe7db4120f5392
Toddnarwal <todd.thomas@comcast.net>
2020-10-18 20:52:33 -0700

    Update watchtower-install.sh

--- install_scripts/watchtower/watchtower-install.sh
+++ install_scripts/watchtower/watchtower-install.sh
@@ -5,6 +5,8 @@ docker run -d \
     --name watchtower \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /etc/timezone:/etc/timezone:ro  \
-    containrrr/watchtower  nextcloud dropbox mariadb \
-    nginx-proxy-manager cloudflare-nextcloud cloudflare-npm \
-    cloudflare-onlyoffice duckdns-gcpdocserver --cleanup 
+    containrrr/watchtower  cloudflare-nextcloud \
+    cloudflare-npm  cloudflare-onlyoffice  \ 
+    nextcloud  dropbox  duckdns-gcpdocserver  \ 
+    mariadb  nginx-proxy-manager  portainer-ce  \
+    --cleanup 
