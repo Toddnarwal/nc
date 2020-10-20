ac8bb4f70c08e1bc6762142635256cd146f97db7
Toddnarwal <todd.thomas@comcast.net>
2020-10-19 17:26:58 -0700

    Update step-ca-install.sh

--- install_scripts/step-ca/step-ca-install.sh
+++ install_scripts/step-ca/step-ca-install.sh
@@ -17,11 +17,14 @@ docker run -d --name step-ca -p 127.0.0.1:9999:9999 -v step:/home/step smallstep
 # Configure local step-cli on the host (not the container)
 wget https://github.com/smallstep/cli/releases/download/v0.15.2/step-cli_0.15.2_amd64.deb 
 sudo dpkg -i step-cli_0.15.2_amd64.deb 
-# Use the root fingerprint from the first container in the following command
+# Use the command below to get root fingerprint from step-ca
+docker exec -it step-ca step certificate fingerprint /home/step/certs/root_ca.crt
+0123456789012345678901234567890123456789012345678901234567890123
+# Use root fingerprint from above command
 step ca bootstrap --ca-url https://localhost:9999 --install --fingerprint 0123456789012345678901234567890123456789012345678901234567890123
 
 # test
-curl https://localhost:9000/health
+curl https://localhost:9999/health
 
 # create certs
 step ca certificate localhost localhost.crt localhost.key
