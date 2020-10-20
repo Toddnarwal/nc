6d0eaf0538f4c3a24177ef95589b204efc4299a6
Toddnarwal <todd.thomas@comcast.net>
2020-10-19 17:18:19 -0700

    Update step-ca-install.sh

--- install_scripts/step-ca/step-ca-install.sh
+++ install_scripts/step-ca/step-ca-install.sh
@@ -1,20 +1,24 @@
 
-# setup step-ca server
+# Setup step-ca server
+# First container (tempory) note the "Root fingerprint" after completing "step ca init"
 docker pull smallstep/step-ca
 docker volume create step
 docker run -it -v step:/home/step smallstep/step-ca sh
 $ step ca init
 ✔ What would you like to name your new PKI?: Smallstep
 ✔ What DNS names or IP addresses would you like to add to your new CA? localhost
-✔ What address will your new CA listen at?: :9000
+✔ What address will your new CA listen at?: :9999
 ✔ What would you like to name the first provisioner for your new CA?: admin
 ✔ What do you want your password to be?: <your password here>
 $ echo <your password here> > /home/step/secrets/password
 
-docker run -d --name step-ca -p 127.0.0.1:9000:9000 -v step:/home/step smallstep/step-ca
-
-# Configure local step-cli 
-step ca bootstrap --ca-url https://localhost:9000 --install --fingerprint f9e45ae9ec5d42d702ce39fd9f3125372ce54d0b29a5ff3016b31d9b887a61a4
+# Second container
+docker run -d --name step-ca -p 127.0.0.1:9999:9999 -v step:/home/step smallstep/step-ca
+# Configure local step-cli on the host (not the container)
+wget https://github.com/smallstep/cli/releases/download/v0.15.2/step-cli_0.15.2_amd64.deb 
+sudo dpkg -i step-cli_0.15.2_amd64.deb 
+# Use the root fingerprint from the first container in the following command
+step ca bootstrap --ca-url https://localhost:9999 --install --fingerprint 0123456789012345678901234567890123456789012345678901234567890123
 
 # test
 curl https://localhost:9000/health
